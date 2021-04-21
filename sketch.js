423bef0c4be83ea3abe384fcf4fbe1aedf496894
SreenandKS <78798957+SreenandKS@users.noreply.github.com>
2021-04-21 14:54:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
-var dog,happyDog,database,foodS,foodStock;
+var image,dog,happyDog,database,foodS,foodStock;
 
 function preload()
 {
-	image = loadImage("images/dogImg.png");
-  happyDog = loadImage("images/dogImg1.png");
+	dogImage = loadImage("dogImg.png");
+  happyDog = loadImage("dogImg1.png");
 }
 
 function setup() {
 	createCanvas(500,500);
-  dog = createSprite(250,250,10,10);
-  dog.addImage(image);
-  database = firebase.database;
-  foodStock = database.ref("Food");
+  dog = createSprite(250,250,150,150);
+  dog.addImage(dogImage);
+  dog.scale = 0.15;
+  database = firebase.database();
+  foodStock = database.ref('Food');
   foodStock.on("value",readStock);
-  
+ 
 }
 
 
@@ -21,18 +22,22 @@ function draw() {
 
   background(46,139,87);
 
-  if(keyWentDown(UP_ARROW)){
+  if(keyDown(UP_ARROW)){
     writeStock(foodS);
     dog.addImage(happyDog);
 
   }
+
+
   drawSprites();
-  
   textSize(20);
   fill(255,144,4);
   stroke(0);
-  text("PRESS UP ARROW KEY TO FEED DRAGO MILK",10,10);
+  text("food left" +  foodS,10,300);
+  text("PRESS UP ARROW KEY TO FEED DRAGO MILK",10,30);
 
+  
+ 
 }
 
 function readStock(data){
