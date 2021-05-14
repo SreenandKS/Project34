b23794bd35ad8bf775956e4e3e582b0a287014fb
SreenandKS <78798957+SreenandKS@users.noreply.github.com>
2021-05-14 16:22:49 +0530

    Add files via upload

---
+++ Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,63 @@
+var image,dog,happyDog,database,foodS,foodStock;
+
+function preload()
+{
+	dogImage = loadImage("dogImg.png");
+  happyDog = loadImage("dogImg1.png");
+}
+
+function setup() {
+ 
+	createCanvas(500,500);
+  database = firebase.database();
+  dog = createSprite(250,250,150,150);
+  dog.addImage(dogImage);
+  dog.scale = 0.15;
+  
+  foodStock = database.ref('Food');
+  foodStock.on("value",readStock);
+ 
+}
+
+
+function draw() {  
+
+  background(46,139,87);
+
+  if(keyDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDog);
+
+  }
+
+
+  drawSprites();
+  textSize(20);
+  fill(255,144,4);
+  stroke(0);
+  text("food left" +  foodS,10,300);
+  text("PRESS UP ARROW KEY TO FEED DRAGO MILK",10,30);
+
+  
+ 
+}
+
+function readStock(data){
+  
+  foodS = data.val();
+}
+
+function writeStock(x){
+
+  if(x <= 0){
+    x = 0;
+  }
+  else{
+    x = x - 1;
+  }
+  database.ref('/').update({
+    Food : x
+  })
+
+  
+}
